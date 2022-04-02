e6d815431a72123e8f88c58a81b2cc3e2a0b6b0f
Harrypatria <46651564+Harrypatria@users.noreply.github.com>
2022-04-02 21:43:01 +0100

    Insert file for R and Power BI

---
+++ R_PowerBI_Library.R
@@ -0,0 +1,43 @@
+# install library: zoo, scales, reshape2, ggplot2, plotly, forecast, lubridate, htmlwidgets, XML
+# Install packages for forecasting: 
+install.packages("zoo")
+install.packages("reshape2")
+install.packages("ggplot2")
+install.packages("plotly")
+install.packages("forecast")
+install.packages("lubridate")
+install.packages("htmlwidgets")
+install.packages("XML")
+install.packages("proto")
+
+# Install packages for clustering: Dbscan, plotly, ggplot2.
+
+library("dbscan")
+library("ggplot2")
+library("plotly")
+
+# Install packages for R 
+# nloptr, seriation, pbkrtest,NbClust, cluster, car, scales, fpc, mclust, apcluster, vegan
+install.packages("nloptr")
+install.packages("seriation")
+install.packages("pbkrtest")
+install.packages("NbClust")
+install.packages("cluster")
+install.packages("car")
+install.packages("scales")
+install.packages("fnc")
+
+library("nloptr")
+library("seriation")
+library("pbkrtest")
+library("NbClust")
+library("cluster")
+library("car")
+library("scales")
+library("fnc")
+
+
+install.packages("DMwR")
+remotes::install_github("cran/DMwR")
+
+
